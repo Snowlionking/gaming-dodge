09548bd8150ad8c929a9b1f792a145aceb9f2c89
Lex Winandy <winandylex_@hotmail.de>
2020-06-18 14:44:48 +0200

    Highscoreservice added

---
+++ src/main/java/services/HighscoreService.java
@@ -0,0 +1,41 @@
+package services;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import game.Game;
+
+public class HighscoreService {
+	
+	public void safeHighscore(String score) {
+		try {
+			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("safeFile.txt", true));
+			bufferedWriter.write(score);
+			bufferedWriter.newLine();
+			bufferedWriter.close();
+			Game.highscoreSet = true;
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+	
+	public List<Integer> loadHighscores() {
+		List<Integer> highscores = new ArrayList<>();
+		try {
+			BufferedReader bufferedReader = new BufferedReader(new FileReader("safeFile.txt"));
+			String line = null;
+			while((line = bufferedReader.readLine()) != null) {
+				highscores.add(Integer.parseInt(line));
+			}
+			bufferedReader.close();
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		return highscores;
+	}
+
+}
